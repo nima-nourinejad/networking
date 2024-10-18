e2c287bb0732f80ef7760fa5b31c72d903a17524
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-10-18 16:25:05 +0300

    update

--- Client.cpp
+++ Client.cpp
@@ -1,11 +1,13 @@
 #include "Client.hpp"
 
-Client::Client(int port) : Socket(port) {
+Client::Client (int port)
+    : Socket (port)
+{
 	_client_fd[0] = _socket_fd;
 	_client_fd[1] = _socket_fd;
 };
-void Client::connectToSocket() {
-	if (connect(_socket_fd, (struct sockaddr *) &_address, sizeof(_address)) == -1)
-		throw std::runtime_error("Failed to connect to socket");
+void Client::connectToSocket ()
+{
+	if (connect (_socket_fd, (struct sockaddr *)&_address, sizeof (_address)) == -1)
+		throw std::runtime_error ("Failed to connect to socket");
 };
-
